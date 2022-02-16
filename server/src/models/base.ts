129539114cbaea5e6711c7c051b11026dad06565
denis <49568323+denissfloww@users.noreply.github.com>
2022-02-16 21:19:35 +0500

    temporary test

--- server/src/models/base.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-export default abstract class Model extends BaseEntity {
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
